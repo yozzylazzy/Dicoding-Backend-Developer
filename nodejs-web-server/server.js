876eaa1ff7702fbb1fbcbabaff718c01fbb45abd
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-04-22 22:00:25 +0700

    Update Latihan Routing Request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,32 +4,33 @@ const requestLiestener = (request, response) => {
     response.setHeader('Content-Type', 'text/html')
     response.statusCode = 200
 
-    const { method } = request
-    if (method === 'GET') {
-        response.end('<h1>Ini adalah method GET</h1>')
+    const { url, method } = request
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>')
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if (method === 'POST') {
+            let body = []
+            request.on('data', (chunk) => {
+                body.push(chunk)
+            })
+            request.on('end', () => {
+                body = Buffer.concat(body).toString()
+                const { name } = JSON.parse(body)
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`)
+            })
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>')
     }
-    if (method === 'POST') {
-        let body = [];
-
-        request.on('data', (chunk) => {
-            body.push(chunk)
-        });
-
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`)
-        });
-
-        // response.end('<h1>Ini adalah method POST</h1>')
-
-    }
-    // if(method === 'PUT'){
-    //     response.end('<h1>Ini adalah method PUT</h1>')
-    // }
-    // if(method === 'DELETE'){
-    //     response.end('<h1>Ini adalah method DELETE</h1>')
-    // }
 }
 
 const server = http.createServer(requestLiestener)
